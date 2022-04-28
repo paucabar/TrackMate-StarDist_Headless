41c75c019fdf35eee1aa21f1192dbe3cfa47b100
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2022-04-28 20:00:05 +0100

    alternative option to create label hyperstack from ROIs

---
+++ Create_Label_Hyperstack_from_ROIs_option_2.groovy
@@ -0,0 +1,23 @@
+#@ ImagePlus imp
+
+import ij.IJ
+import ij.plugin.frame.RoiManager
+
+impLabel = IJ.createImage("Labeling", "16-bit black", imp.getWidth(), imp.getHeight(), 1, imp.getNSlices(), imp.getNFrames())
+rm = RoiManager.getInstance()
+rm.getRoisAsArray().eachWithIndex { roi, index ->
+    def pos
+    if (roi.hasHyperStackPosition()) {
+        // Completely untested!
+        pos = impLabel.getStackIndex(roi.getCPosition(), roi.getZPosition(), roi.getZPosition())
+    } else {
+        pos = roi.getPosition()     
+    }
+    def ip = impLabel.getStack().getProcessor(pos)
+    ip.setColor(index+1)
+    ip.fill(roi)
+}
+
+imp.setDisplayRange(0, rm.getCount())
+IJ.run(impLabel, "glasbey inverted", "")
+impLabel.show()
