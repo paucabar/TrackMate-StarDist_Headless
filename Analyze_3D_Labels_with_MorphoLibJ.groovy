baa4bfc783448c80419c019233925a47dcab87dc
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2022-05-06 12:31:17 +0100

    Use MorphoLibJ API insted of ImageJ run command

--- Analyze_3D_Labels_with_MorphoLibJ.groovy
+++ Analyze_3D_Labels_with_MorphoLibJ.groovy
@@ -1,12 +1,16 @@
 #@ ImagePlus imp
 
-import ij.IJ
-
-// Swap T and Z dimensions if Z=1
+// Swap Z and T dimensions if Z=1
 dims = imp.getDimensions() // default order: XYCZT
 if (dims[3] == 1) {
 	imp.setDimensions( dims[2,4,3] )
 }
 
-IJ.run("Analyze Regions 3D", "centroid surface_area_method=[Crofton (13 dirs.)] euler_connectivity=6")
-IJ.renameResults("Results")
+import inra.ijpb.plugins.AnalyzeRegions3D
+
+println imp.getClass()
+
+// analyze 3D labels with MorphoLibJ
+ar3D = new AnalyzeRegions3D()
+def table = ar3D.process(imp)
+table.show("Results")
